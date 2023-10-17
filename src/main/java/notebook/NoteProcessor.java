e0e213cb201dbddb11b362e1b43846112f444e58
aliya <aliya.gabbassova1911@gmail.com>
2023-10-17 13:37:21 +0600

    First task

--- src/main/java/notebook/NoteProcessor.java
+++ src/main/java/notebook/NoteProcessor.java
@@ -1,2 +1,28 @@
-package Notebook;public class NoteProcessor {
+package notebook;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class NoteProcessor {
+    public static void processAndWriteToFile(String note) {
+        String formattedNote = formatNoteWithTimestamp(note);
+
+        try (FileWriter fileWriter = new FileWriter("notes.txt", true)) {
+            fileWriter.write(formattedNote + "\n");
+            System.out.println("Дозапись в файл: " + formattedNote);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String formatNoteWithTimestamp(String note) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String timestamp = dateFormat.format(new Date());
+
+        return timestamp + " -> " + note;
+    }
 }
+
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,14 @@
-package org.example;public class Main {
+package org.example;
+import notebook.NoteProcessor;
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите заметку: ");
+        String note = scanner.nextLine();
+
+        NoteProcessor.processAndWriteToFile(note);
     }
 }
